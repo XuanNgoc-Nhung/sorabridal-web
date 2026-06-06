8fbfbef221760dfe53bd6e33c1bad59945b13374
Xuanngoc <xuanngoc10a@gmail.com>
2026-06-06 14:04:25 +0700

    css section about

--- resources/views/user/home/sections/about.blade.php
+++ resources/views/user/home/sections/about.blade.php
@@ -1,9 +1,14 @@
 <!-- ══════════════════════ ABOUT ═════════════════════════ -->
 <section class="home-section">
     <div class="container">
-        <div class="row align-items-center g-4 g-lg-5">
+        <div class="row about-sec-tag-row">
+            <div class="col-12 text-center about-sec-tag-wrap sr">
+                <span class="sec-tag d-block mb-0">Lời nhắn đầu tiên</span>
+            </div>
+        </div>
 
-            <div class="col-lg-6 about-img-wrap sr sr-left sr-blur-in order-lg-1 order-2">
+        <div class="row align-items-center g-4 g-lg-5 about-row">
+            <div class="col-lg-6 about-img-wrap sr sr-left sr-blur-in order-1 order-lg-1">
                 <div class="about-img-main">
                     <img src="{{ asset('/user/anhFouder.webp') }}" style="width: 100%; height: 100%; object-fit: cover;"
                         alt="Founder Sora Studio">
@@ -11,32 +16,31 @@
                 {{-- <div class="about-img-accent">
           <img src="{{ asset('/user/demo.jpg') }}?t=190x190/ecdacf/b07d62?text=Award" alt="Award">
             </div> --}}
-        </div>
+            </div>
 
-        <div class="col-lg-6 about-content sr sr-right d1 sr-flip order-lg-2 order-1">
-            <span class="sec-tag d-block">Lời nhắn đầu tiên</span>
-            <h2 class="sec-title mb-3">Từ trái tim Sora</h2>
-            {{-- <div class="about-pills">
+            <div class="col-lg-6 about-content sr sr-right d1 sr-flip order-2 order-lg-2">
+                <h2 class="sec-title mb-3">Từ trái tim Sora</h2>
+                {{-- <div class="about-pills">
                     <span class="pill">Fine Art Photography</span>
                     <span class="pill">10+ Năm kinh nghiệm</span>
                     <span class="pill">Sora Rooftop & Villa</span>
                 </div> --}}
-            <div class="quote-open">"</div>
-            <p class="about-quote">Tại Sora Bridal, chúng tôi không chỉ thực hiện một bộ ảnh hay một lễ cưới, mà
-                chúng tôi đặt trọn tâm huyết như đang chăm chút cho ngày trọng đại của chính mình, để mỗi trải
-                nghiệm và từng sản phẩm tới tay khách hàng đều là sự kết tinh hoàn hảo nhất dành cho ngày hạnh phúc
-                của hai bạn!
-                <br>
-                Sora Bridal Tận Tâm - Trách Nhiệm - Phát Triển.</p>
-            <div class="about-sig">
-                <div class="sig-line"></div>
-                <div>
-                    <div class="sig-name">Đỗ Tuấn Anh</div>
-                    <div class="sig-role">Founder Sora Bridal</div>
+                <div class="quote-open">"</div>
+                <p class="about-quote">Tại Sora Bridal, chúng tôi không chỉ thực hiện một bộ ảnh hay một lễ cưới, mà
+                    chúng tôi đặt trọn tâm huyết như đang chăm chút cho ngày trọng đại của chính mình, để mỗi trải
+                    nghiệm và từng sản phẩm tới tay khách hàng đều là sự kết tinh hoàn hảo nhất dành cho ngày hạnh phúc
+                    của hai bạn!
+                    <br>
+                    Sora Bridal Tận Tâm - Trách Nhiệm - Phát Triển.</p>
+                <div class="about-sig">
+                    <div class="sig-line"></div>
+                    <div>
+                        <div class="sig-name">Đỗ Tuấn Anh</div>
+                        <div class="sig-role">Founder Sora Bridal</div>
+                    </div>
                 </div>
             </div>
-        </div>
 
-    </div>
+        </div>
     </div>
 </section>
